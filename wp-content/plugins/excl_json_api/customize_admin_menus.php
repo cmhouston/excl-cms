0f1dfa79afcb4254140c9c186446b75eef226e38
Sean Fisher <seandavidfisher@gmail.com>
2014-08-07 11:57:22 -0500

    Added code to reorder the admin content editing screen's meta boxes to how we want them

--- wp-content/plugins/excl_json_api/customize_admin_menus.php
+++ wp-content/plugins/excl_json_api/customize_admin_menus.php
@@ -1,6 +1,9 @@
 <?php
+/**********************************************************
+ * General
+ *********************************************************/
+
 function remove_menus(){
-  
   remove_menu_page( 'edit.php' );                   //Posts
   remove_menu_page( 'edit.php?post_type=page' );    //Pages
   remove_menu_page( 'link-manager.php' );			//Links
@@ -41,3 +44,64 @@ function edit_admin_menus() {
     $submenu['edit.php?post_type=component-post'][15][0] = 'Sections'; // Rename categories to meal types
 }
 add_action( 'admin_menu', 'edit_admin_menus' );
+
+
+/**********************************************************
+ * Reording the meta boxes on the content editing pages
+ *********************************************************/
+
+// Adapted from plugin "Post Meta Box Order" (http://github.com/LettoBlog/post-meta-box-order) by Mustafa Uysal, LettoBlog (http://lettoblog.com)
+
+$types = array('museum', 'exhibit', 'component', 'component-post');
+
+add_action('init', 'posts_widgets_order');
+
+function posts_widgets_order() {
+    global $wpdb, $user_ID;
+
+	if ( !empty( $user_ID ) ) {
+		foreach ($types as $type) {
+			/**
+			 * Default meta boxes
+			 * submitdiv - publish
+			 * categorydiv - category
+			 * tagsdiv-post_tag - tags
+			 * postimagediv -  featured image
+			 * authordiv - author
+			 * postexcerpt -  excerpt
+			 * commentstatusdiv - Discussion
+			 * ........
+			 *
+			 * If you want to order any plugin's meta box use meta box's $id
+			 */
+			
+			//Left Column
+			$posts_widgets_order_left_column[] = 'wpcf-post-relationship';
+			$posts_widgets_order_left_column[] = 'wpcf-group-' . $type . '-custom-fields';
+			$posts_widgets_order_left_column[] = 'commentstatusdiv';
+
+			//Right Column
+			$posts_widgets_order_right_column[] = 'submitdiv';
+			$posts_widgets_order_right_column[] = 'categorydiv';
+		
+			$left_column = '';
+			foreach ( $posts_widgets_order_left_column as $posts_widgets_order_left_column_widget ) {
+			    $left_column .= $posts_widgets_order_left_column_widget . ',';
+			}
+			$left_column = rtrim($left_column, ',');
+
+			$right_column = '';
+			foreach ( $posts_widgets_order_right_column as $posts_widgets_order_right_column_widget ) {
+			    $right_column .= $posts_widgets_order_right_column_widget . ',';
+			}
+			$right_column = rtrim($right_column, ',');
+
+			$posts_widget_order = array();
+
+			$posts_widget_order['side'] = $right_column;
+			$posts_widget_order['normal'] = $left_column;
+
+			update_user_option($user_ID, 'meta-box-order_' . $type, $posts_widget_order, true);
+		}
+	}
+}
